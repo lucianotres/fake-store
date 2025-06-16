e499f1dd8e8beca9b23e5c0b7ce5f76d64b1bcc7
Luciano Tres <lucianotres@outlook.com>
2025-06-16 15:27:10 -0300

    Adicionado servico para consumo da API FakeStoreApi. Novo model para Produto. Listagem basica de produtos na tela inicial para teste da API

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using FakeProduct;
+using FakeProduct.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("fakestore", http => http.BaseAddress = new Uri("https://fakestoreapi.com/"));
+builder.Services.AddScoped<FakeStoreService>();
 
 await builder.Build().RunAsync();
---
+++ Services/FakeStoreService.cs
@@ -0,0 +1,35 @@
+﻿using FakeProduct.Models;
+using System.Net.Http.Json;
+
+namespace FakeProduct.Services;
+
+public class FakeStoreService
+{
+    private ILogger<FakeStoreService> _log;
+    private IHttpClientFactory _httpClientFactory;
+
+    public FakeStoreService(ILogger<FakeStoreService> logger, IHttpClientFactory httpClientFactory)
+    {
+        _log = logger;
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<List<Product>> GetProductsAsync()
+    {
+        _log.LogInformation("Buscando lista de produtos...");
+        var client = _httpClientFactory.CreateClient("fakestore");
+        var response = await client.GetAsync("/products");
+
+        if (!response.IsSuccessStatusCode)
+        {
+            _log.LogError($"Falha {response.StatusCode}: não pode retornar lista de produtos");
+            return new ();
+        }
+
+        var products = await response.Content.ReadFromJsonAsync<List<Product>>() ?? new ();
+        
+        _log.LogInformation($"Retornado {products.Count} produto(s)");
+        return products;
+    }
+
+}
