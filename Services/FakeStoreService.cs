073d972e986a32a8a656bc84f61214f48723f2e4
Luciano Tres <lucianotres@outlook.com>
2025-06-16 16:25:53 -0300

    Implementado restante dos métodos em /products Feito uma tela inicial para ver e alterar produtos.

--- Services/FakeStoreService.cs
+++ Services/FakeStoreService.cs
@@ -1,4 +1,5 @@
 ﻿using FakeProduct.Models;
+using System.ComponentModel;
 using System.Net.Http.Json;
 
 namespace FakeProduct.Services;
@@ -32,4 +33,79 @@ public class FakeStoreService
         return products;
     }
 
+    public async Task<Product?> GetProductAsync(int id)
+    {
+        _log.LogInformation($"Buscando produto com ID {id}...");
+        var client = _httpClientFactory.CreateClient("fakestore");
+        var response = await client.GetAsync($"/products/{id}");
+
+        if (!response.IsSuccessStatusCode)
+        {
+            _log.LogError($"Falha {response.StatusCode}: não pode retornar produto com ID {id}");
+            return null;
+        }
+
+        var product = await response.Content.ReadFromJsonAsync<Product>();
+
+        if (product == null)
+        {
+            _log.LogWarning($"Produto com ID {id} não encontrado");
+            return null;
+        }
+
+        _log.LogInformation($"Retornado produto {id}.");
+        return product;
+    }
+
+    public async Task<Product?> PostProductAsync(Product product)
+    {
+        _log.LogInformation("Criando novo produto...");
+        var client = _httpClientFactory.CreateClient("fakestore");
+        var response = await client.PostAsJsonAsync("/products", product);
+        
+        if (!response.IsSuccessStatusCode)
+        {
+            _log.LogError($"Falha {response.StatusCode}: não pode criar produto");
+            return null;
+        }
+
+        var newProduct = await response.Content.ReadFromJsonAsync<Product>();
+
+        _log.LogInformation($"Produto criado com sucesso, novo ID: {newProduct?.Id}");
+        return newProduct;
+    }
+
+    public async Task<Product?> PutProductAsync(Product product)
+    {
+        _log.LogInformation($"Atualizando produto com ID {product.Id}...");
+        var client = _httpClientFactory.CreateClient("fakestore");
+        var response = await client.PutAsJsonAsync($"/products/{product.Id}", product);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            _log.LogError($"Falha {response.StatusCode}: não pode atualizar produto com ID {product.Id}");
+            return null;
+        }
+
+        var updatedProduct = await response.Content.ReadFromJsonAsync<Product>();
+
+        _log.LogInformation($"Produto com ID {updatedProduct?.Id} atualizado com sucesso.");
+        return updatedProduct;
+    }
+
+    public async Task<bool> DeleteProductAsync(int id)
+    {
+        _log.LogInformation($"Excluindo produto com ID {id}...");
+        var client = _httpClientFactory.CreateClient("fakestore");
+        var response = await client.DeleteAsync($"/products/{id}");
+        
+        if (!response.IsSuccessStatusCode)
+        {
+            _log.LogError($"Falha {response.StatusCode}: não pode excluir produto com ID {id}");
+            return false;
+        }
+
+        _log.LogInformation($"Produto com ID {id} excluído com sucesso.");
+        return true;
+    }
 }
