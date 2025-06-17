7a80e4385387e866cb7893ab4d5bbd0835282d78
Luciano Tres <lucianotres@outlook.com>
2025-06-17 11:07:21 -0300

    Ajustado visual da tela de usuários

--- Models/User.cs
+++ Models/User.cs
@@ -1,8 +1,16 @@
-﻿namespace FakeProduct.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FakeProduct.Models;
 public class User
 {
     public int Id { get; set; } = 0;
+
+    [Required(ErrorMessage = "Obrigatório informar nome de usuário!")]
     public string Username { get; set; } = string.Empty;
+
+    [EmailAddress(ErrorMessage = "e-mail inválido!")]
     public string? Email { get; set; }
+
+    [Required(ErrorMessage = "Obrigatório informar o e-mail!")]
     public string? Password { get; set; }
 }
