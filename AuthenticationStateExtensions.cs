f239f87eab5e4deccc8f9dd7ddde41fcf89a293f
Luciano Tres <lucianotres@outlook.com>
2025-06-17 15:59:19 -0300

    Correção do título das páginas.
    Na listagem de carrinhos agora permite adicionar novo.
    Criado extension para buscar o id o usuário via Claim.
    Marcando "meu" nos carrinhos que são do usuário atualmente logado.

---
+++ AuthenticationStateExtensions.cs
@@ -0,0 +1,17 @@
+﻿using FakeProduct.Models;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace FakeProduct;
+
+public static class AuthenticationStateExtensions
+{
+    public static int? GetUserId(this AuthenticationState state)
+    {
+        var claim = state.User.Claims.FirstOrDefault(w => w.Type == ClaimTypes.NameIdentifier);
+        if (claim != null && int.TryParse(claim.Value, out int id))
+            return id;
+        else
+            return null;
+    }
+}
